9306302698bc17cb3c687c7beba1ce72095be784
Utkarsh  Jain <utkarsh.jain@intimetec.com>
2024-11-05 09:51:17 +0530

    Added all files

---
+++ Restaurant_API/Restaurant_API/Controllers/MenuItemController.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Restaurant_API.Data;
+using Restaurant_API.Models;
+using Restaurant_API.Models.Dto;
+using System;
+using System.Net;
+
+namespace Restaurant_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private ApiResponse _response;
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _response = new ApiResponse();
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}", Name = "GetMenuItem")]
+        public async Task<IActionResult> GetMenuItem(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
+            }
+
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+
+            if (menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+            _response.Result = menuItem;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MenuItem>> CreateMenuItem([FromForm] MenuItemCreateDTO menuItemCreateDTO)
+        {
+            if (menuItemCreateDTO.File.Length > 1 * 1024 * 1024)
+            {
+                ModelState.AddModelError("CustomError", "File Size More than 1 MB");
+                return BadRequest();
+            }
+            else
+            {
+                string[] allowedFileExtentions = [".jpg", ".jpeg", ".png"];
+                if (menuItemCreateDTO.File == null)
+                {
+                    ModelState.AddModelError("CustomError", "Please Upload Image");
+                    return BadRequest();
+                }
+                else
+                {
+                    var contentPath = _webHostEnvironment.ContentRootPath;
+                    var path = Path.Combine(contentPath, "images/menuitem");
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    // Check the allowed extenstions
+                    var ext = Path.GetExtension(menuItemCreateDTO.File.FileName);
+                    if (!allowedFileExtentions.Contains(ext))
+                    {
+                        ModelState.AddModelError("CustomError", "Unsupported Image File");
+                        return BadRequest();
+                    }
+                    else
+                    {
+                        // generate a unique filename
+                        var fileName = $"{Guid.NewGuid().ToString()}{ext}";
+                        var fileNameWithPath = Path.Combine(path, fileName);
+                        using var stream = new FileStream(fileNameWithPath, FileMode.Create);
+                        menuItemCreateDTO.File.CopyTo(stream);
+                        string createdImage = fileName;
+
+                        var menuItem = new MenuItem
+                        {
+                            Name = menuItemCreateDTO.Name,
+                            Description = menuItemCreateDTO.Description,
+                            SpecialTag = menuItemCreateDTO.SpecialTag,
+                            Category = menuItemCreateDTO.Category,
+                            Price = menuItemCreateDTO.Price,
+                            Image = createdImage,
+
+                        };
+                        _db.MenuItems.Add(menuItem);
+                        _db.SaveChanges();
+                        return CreatedAtRoute("GetMenuItem", new { id = menuItem.Id }, menuItem);
+                    }
+                }
+            }
+
+
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateMenuItem(int id, [FromForm] MenuItemUpdateDTO menuItemUpdateDTO)
+        {
+            if (id != menuItemUpdateDTO.Id && menuItemUpdateDTO == null)
+            {
+                return BadRequest();
+            }
+
+            MenuItem menuItemFromDB = _db.MenuItems.Find(id);
+            if (menuItemFromDB == null)
+            {
+                return NotFound();
+            }
+
+            menuItemFromDB.Name = menuItemUpdateDTO.Name;
+            menuItemFromDB.Description = menuItemUpdateDTO.Description;
+            menuItemFromDB.SpecialTag = menuItemUpdateDTO.SpecialTag;
+            menuItemFromDB.Category = menuItemUpdateDTO.Category;
+            menuItemFromDB.Price = menuItemUpdateDTO.Price;
+
+            string oldImage = menuItemFromDB.Image;
+
+            if (menuItemUpdateDTO.File != null && menuItemUpdateDTO.File.Length > 0)
+            {
+                var contentPath = _webHostEnvironment.ContentRootPath;
+                var path = Path.Combine(contentPath, $"images/menuitem", oldImage);
+                System.IO.File.Delete(path);
+
+                if (menuItemUpdateDTO.File.Length > 1 * 1024 * 1024)
+                {
+                    ModelState.AddModelError("CustomError", "File Size More than 1 MB");
+                    return BadRequest();
+                }
+                string[] allowedFileExtentions = [".jpg", ".jpeg", ".png"];
+                if (menuItemUpdateDTO.File == null)
+                {
+                    ModelState.AddModelError("CustomError", "Please Upload Image");
+                    return BadRequest();
+                }
+                else
+                {
+                    contentPath = _webHostEnvironment.ContentRootPath;
+                    path = Path.Combine(contentPath, "images/menuitem");
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    // Check the allowed extenstions
+                    var ext = Path.GetExtension(menuItemUpdateDTO.File.FileName);
+                    if (!allowedFileExtentions.Contains(ext))
+                    {
+                        ModelState.AddModelError("CustomError", "Unsupported Image File");
+                        return BadRequest();
+                    }
+                    else
+                    {
+                        // generate a unique filename
+                        var fileName = $"{Guid.NewGuid().ToString()}{ext}";
+                        var fileNameWithPath = Path.Combine(path, fileName);
+                        using var stream = new FileStream(fileNameWithPath, FileMode.Create);
+                        menuItemUpdateDTO.File.CopyTo(stream);
+                        string createdImage = fileName;
+
+                        menuItemFromDB.Image = createdImage;
+
+                    }
+
+                    _db.MenuItems.Update(menuItemFromDB);
+                    _db.SaveChanges();
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteMenuItem(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            MenuItem menuItemTobeDeleted = _db.MenuItems.Find(id);
+            if (menuItemTobeDeleted == null)
+            {
+                return BadRequest();
+            }
+
+            string deletedImage = menuItemTobeDeleted.Image;
+
+            var contentPath = _webHostEnvironment.ContentRootPath;
+            var path = Path.Combine(contentPath, $"images/menuitem", deletedImage);
+            System.IO.File.Delete(path);
+
+            _db.MenuItems.Remove(menuItemTobeDeleted);
+            _db.SaveChanges();
+
+            return NoContent();
+
+
+        }
+    }
+
+}
---
+++ Restaurant_API/Restaurant_API/Controllers/PaymentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Razorpay.Api;
+using Restaurant_API.Data;
+using Restaurant_API.Models;
+using System.Net;
+
+namespace Restaurant_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        protected ApiResponse _response;
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _db;
+
+        public PaymentController(IConfiguration configuration, ApplicationDbContext db)
+        {
+            _configuration = configuration;
+            _db = db;
+            _response = new();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> MakePayment(string userId)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u => u.CartItems).
+                ThenInclude(u => u.MenuItem).FirstOrDefault(u => u.UserId == userId);
+
+            if (shoppingCart == null || shoppingCart.CartItems == null || shoppingCart.CartItems.Count() == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest();
+            }
+
+            #region Create Payment Intent
+
+            shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
+
+            Dictionary<string, object> input = new Dictionary<string, object>();
+            input.Add("amount",(int)(shoppingCart.CartTotal * 100));
+            input.Add("currency", "INR");
+            input.Add("receipt", "12121");
+
+            string key = _configuration["RazorpaySettings:KeyId"];
+            string secret = _configuration["RazorpaySettings:SecretKey"];
+
+            RazorpayClient client = new RazorpayClient(key, secret);
+
+            Razorpay.Api.Order order = client.Order.Create(input);
+            string orderId = order["id"].ToString();
+            shoppingCart.RazorpayPaymentIntentId = orderId;
+           
+
+            #endregion
+
+            _response.Result = shoppingCart;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
